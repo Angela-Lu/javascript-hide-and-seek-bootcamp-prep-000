0474e7332e5f2fec122149e6263c4a60e5a778ea
Angela Lu <angela.lu0919@gmail.com>
2018-09-27 01:30:30 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
+function getFirstSelector(selector) {
+  return querySelector(selector)
+}
 
+function nestedTarget() {
+  document.querySelector('#nested .target')
+}
+
+
+function increaseRankBy(n) {
+  const x = document.querySelectorAll('.ranked-list');
+
+  for (let i = 0; i < x.length; i++) {
+    let y = x[i].children;
+
+    for (let j = 0; j < y.length; j++) {
+     y[j].innerHTML = parseInt(y[j].innerHTML) + n;
+    }
+  }
+}
+
+function deepestChild() {
+  let node = document.getElementById('grand-node');
+  let nextNode = node.children[0];
+
+  while (nextNode) {
+    node = nextNode;
+    nextNode = node.children[0];
+  }
+
+  return node;
+}
