adc611b21b4359cbd2b8a00f052d0c6b6444859d
Angela Lu <angela.lu0919@gmail.com>
2018-09-27 01:12:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,14 +7,19 @@ function nestedTarget() {
 }
 
 
-
 function increaseRankBy(n) {
   const rankedLists = document.querySelectorAll('.ranked-list');
 
   for (let i = 0, l = rankedLists.length; i < l; i++) {
     let children = rankedLists[i].children;
 
-    fofunction deepestChild() {
+    for (let j = 0, k = children.length; j < k; j++) {
+      children[j].innerHTML = parseInt(children[j].innerHTML) + n;
+    }
+  }
+}
+
+function deepestChild() {
   let node = document.getElementById('grand-node');
   let nextNode = node.children[0];
 
@@ -24,8 +29,4 @@ function increaseRankBy(n) {
   }
 
   return node;
-}r (let j = 0, k = children.length; j < k; j++) {
-      children[j].innerHTML = parseInt(children[j].innerHTML) + n;
-    }
-  }
 }
